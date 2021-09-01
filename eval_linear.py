dd33a7de95afa2a321164962950d56e9b4b9c040
mattroos <matt.roos@yahoo.com>
2021-09-01 12:23:36 -0600

    Test commit, using pycharm

--- eval_linear.py
+++ eval_linear.py
@@ -27,6 +27,7 @@ from torchvision import models as torchvision_models
 import utils
 import vision_transformer as vits
 
+# Test comment for learning how to use pycharm
 
 def eval_linear(args):
     utils.init_distributed_mode(args)
