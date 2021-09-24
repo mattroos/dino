0d7bd76ab7ef83d3fd8858e5d277520c0faeeee1
mattroos <matt.roos@yahoo.com>
2021-09-24 14:16:27 -0600

    include linear head model

--- grid_search.sh
+++ grid_search.sh
@@ -6,52 +6,64 @@ cd $WORK_DIR
 
 # Loop over several ViT-Base models with different MLP heads
 
-declare -a N_LAYERS=("1" "2")
+# declare -a N_LAYERS=("1" "2")
 # declare -a N_NODES=("40" "400")
+# declare -a HIDDEN_ACT=("gelu" "relu")
+declare -a TYPE=("linear" "mlp")
+declare -a N_LAYERS=("1")
 declare -a N_NODES=("40")
-declare -a HIDDEN_ACT=("gelu" "relu")
+declare -a HIDDEN_ACT=("relu")
 
-for n_layers in "${N_LAYERS[@]}"
+for type in "${TYPE[@]}"
 do
-    for n_nodes in "${N_NODES[@]}"
+    for n_layers in "${N_LAYERS[@]}"
     do
-        for hidden_act in "${HIDDEN_ACT[@]}"
+        for n_nodes in "${N_NODES[@]}"
         do
-            # echo -n "dummy.py "
-            # echo -n "--n_hidden_layers $n_layers "
-            # echo "--n_hidden_nodes $n_nodes "
+            for hidden_act in "${HIDDEN_ACT[@]}"
+            do
+                # echo -n "dummy.py "
+                # echo -n "--n_hidden_layers $n_layers "
+                # echo "--n_hidden_nodes $n_nodes "
 
-            # python dummy.py \
-            # --n_hidden_layers $n_layers \
-            # --n_hidden_nodes $n_nodes
+                # python dummy.py \
+                # --n_hidden_layers $n_layers \
+                # --n_hidden_nodes $n_nodes
 
-            OUTPUT_NAME=scale_head_base_mlp_L"$n_layers"_N"$n_nodes"_HIDACT"$hidden_act"
-            mkdir ./$OUTPUT_NAME
+                if test "$type" = 'linear'
+                then
+                    OUTPUT_NAME=scale_head_base_"$type"
+                else
+                    # Assuming MLP head
+                    OUTPUT_NAME=scale_head_base_"$type"_L"$n_layers"_N"$n_nodes"_HIDACT"$hidden_act"
+                fi
 
-            # python dummy.py \
-            python -m torch.distributed.launch --nproc_per_node=4 eval_linear_scale.py \
-            --arch vit_base \
-            --n_last_blocks 1 \
-            --patch_size 8 \
-            --avgpool_patchtokens True \
-            --batch_size_per_gpu 128 \
-            --data_path /workspace/Downloads/Flickr_cows_train_val_sets/ \
-            --num_workers 8 \
-            --head_type mlp \
-            --output_act softplus \
-            --hidden_act $hidden_act \
-            --n_hidden_layers $n_layers \
-            --n_hidden_nodes $n_nodes \
-            --output_dir ./$OUTPUT_NAME \
-            2>/dev/null
+                mkdir ./$OUTPUT_NAME
 
-            echo ""
+                # python dummy.py \
+                python -m torch.distributed.launch --nproc_per_node=4 eval_linear_scale.py \
+                --arch vit_base \
+                --n_last_blocks 1 \
+                --patch_size 8 \
+                --avgpool_patchtokens True \
+                --batch_size_per_gpu 128 \
+                --data_path /workspace/Downloads/Flickr_cows_train_val_sets/ \
+                --num_workers 8 \
+                --output_act softplus \
+                --head_type $type \
+                --hidden_act $hidden_act \
+                --n_hidden_layers $n_layers \
+                --n_hidden_nodes $n_nodes \
+                --output_dir ./$OUTPUT_NAME \
+                2>/dev/null
 
-            # Tar and copy results to AWS S3
-            tar -cvf $OUTPUT_NAME.tar ./$OUTPUT_NAME
-            aws s3 cp $OUTPUT_NAME.tar s3://bicog-datasets/cows/$OUTPUT_NAME.tar
+                echo ""
 
+                # Tar and copy results to AWS S3
+                tar -cvf $OUTPUT_NAME.tar ./$OUTPUT_NAME
+                aws s3 cp $OUTPUT_NAME.tar s3://bicog-datasets/cows/$OUTPUT_NAME.tar
+
+            done
         done
     done
 done
-
